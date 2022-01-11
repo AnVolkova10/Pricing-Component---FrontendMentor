70bfde1d2f7de471292f22cb8420057fe19caed2
AnVolkova10 <curzi.angela@gmail.com>
2022-01-11 11:21:25 -0300

    bases

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
+  
+// execute the compiled template and print the output to the console
+  console.log(template({ doesWhat: "rocks!" }));
