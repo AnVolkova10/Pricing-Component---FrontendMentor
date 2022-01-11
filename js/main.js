5d025aebddbc5f8d9e9442be5ca159cc8a516fdc
AnVolkova10 <curzi.angela@gmail.com>
2022-01-11 14:05:20 -0300

    comienzo de sass y handlebars para las cards

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
-  
-// execute the compiled template and print the output to the console
-  console.log(template({ doesWhat: "rocks!" }));
+$(document).ready(function() {    
+    // Retrieve the template data from the HTML (jQuery is used here).    
+    var template = $('#basic-template1').html();    
+
+    // Compile the template data into a function    
+    var templateScript = Handlebars.compile(template);    
+    // Define data in JSON format.    
+    
+    var json =[ {"Type":"Basic","PriceAnnually":"199.99","PriceMonthly":"19.99","Storage":"500 GB","UsersAllowed":"2","UploadSize":"3","Active":false },
+            {"Type":"Professional","PriceAnnually":"249.99","PriceMonthly":"24.99","Storage":"1 TB","UsersAllowed":"5","UploadSize":"10", "Active": true},
+            {"Type":"Master","PriceAnnually":"399.99","PriceMonthly":"39.99","Storage":"2 TB","UsersAllowed":"10","UploadSize":"20", "Active": false}
+    ]
+    // Pass Data to template script.    
+    var html = templateScript(json);    
+    
+    // Insert the HTML code into the page    
+    $(document.body).append(html);    
+});
+
+
