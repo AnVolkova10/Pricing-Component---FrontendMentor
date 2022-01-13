34c4cfc64f16948caa2ae34bce8a3703f7b3e3fe
AnVolkova10 <curzi.angela@gmail.com>
2022-01-13 00:12:14 -0300

    95% checkbox working

--- js/main.js
+++ js/main.js
@@ -4,17 +4,31 @@ $(document).ready(function() {
 
     // Compile the template data into a function    
     var templateScript = Handlebars.compile(template);    
-    // Define data in JSON format.    
     
-    var json =[ {"Type":"Basic","PriceAnnually":"199.99","PriceMonthly":"19.99","Storage":"500 GB","UsersAllowed":"2","UploadSize":"3","Active":false },
+    // Define data in JSON format.    
+    var json =[ 
+            {"Type":"Basic","PriceAnnually":"199.99","PriceMonthly":"19.99","Storage":"500 GB","UsersAllowed":"2","UploadSize":"3","Active":false },
             {"Type":"Professional","PriceAnnually":"249.99","PriceMonthly":"24.99","Storage":"1 TB","UsersAllowed":"5","UploadSize":"10", "Active": true},
             {"Type":"Master","PriceAnnually":"399.99","PriceMonthly":"39.99","Storage":"2 TB","UsersAllowed":"10","UploadSize":"20", "Active": false}
     ]
+
+    $(function () {
+        $("#checkbox").click(function () {
+            if ($(this).is(":checked")) {
+                $(".monthly").show();
+                $(".annually").hide();
+            } else {
+                $(".annually").show();
+                $(".monthly").hide();
+            }
+        });
+    });
+    
+    
     // Pass Data to template script.    
     var html = templateScript(json);    
     
     // Insert the HTML code into the page    
     $(document.body).append(html);    
 });
-
 
